b52008da16b0e7381fe97060627dc57c061e2aeb
Unknown <tgdubuke@wpi.edu>
2018-09-19 13:37:22 -0400

    Merged with my tables

--- p1.sql
+++ p1.sql
@@ -39,3 +39,46 @@ create table WineCompRep (
     Constraint Rep_User_FK Foreign Key (username) references Account(username)
     );
     
+create table Wines(
+    wineID number, 
+    brand varchar2(25),
+    alcContent number(4, 1),
+    bottlerName varchar(25),
+    netContent number(5, 1),
+    class varchar(25),
+    appellation varhchar(25)
+);
+
+create Sequence wineID_seq start with 100 increment by 1;
+
+create table WineLabelForm(
+    formID number,
+    currentReviewerID number,
+    dateSubmitted date,
+    dateRejected date,
+    dateAccpeted date,
+    status varchar2(25),
+    wineID number,
+    vintage number(4, 0),
+    ttbID number,
+    repID number,
+    dateBegan date,
+    comments varchar2(100),
+    year number(4, 0)
+    Constraint WineLabelForm_PK Primary Key(formID),
+    Constraint WineLabelForm_FK_1 Foreign Key (wineID) References (WineCompRep.wineID),
+    Constraint WineLabelForm_FK_2 Foreign Key (ttbID) References (SuperAgent.ttbID),
+    Constraint WineLabelForm_FK_3 Foreign Key (repID) References (WineCompRep.repID)
+);
+
+create Sequence formID_seq start with 100 increment by 1;
+
+create table AgentWorksOn(
+    username varchar2(25),
+    formID number,
+    Constraint AgentWorksOn_PK Primary Key (username, formID),
+    Constraint AgentWorksOn_FK_1 Foreign Key (username) References (GovAgent.username),
+    Constraint AgentWorksOn_FK_2 Foreign Key (formID) References (WineLabelForm.formID)
+);
+    
+    
